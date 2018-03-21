fd4edc4504c33b2e70487d038eb51c89b38f84a2
shenjingbaoma <2795505031@qq.com>
2018-03-21 13:34:14 +0800

    haha

---
+++ app/src/main/java/com/example/administrator/rubbish01/Demo4Activity.java
@@ -0,0 +1,13 @@
+package com.example.administrator.rubbish01;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Demo4Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_demo4);
+    }
+}
